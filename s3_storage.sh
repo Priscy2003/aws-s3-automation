8e8ae1bbeac51abc524a2833af28bbf592a1d3dc
Priscy <pristine535@gmail.com>
2025-11-04 10:27:18 +0100

    Initial commit

---
+++ s3_storage.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+# Simple Cloud Storage File (AWS S3)
+# Individual Project- Group 2
+# By Prisca Austine
+
+BUCKET_NAME="prissy-771-storage"
+REGION="us-east-1"
+LOG_FILE="s3_log.txt"
+
+echo "Creating bucket..."
+aws s3api create-bucket --bucket $BUCKET_NAME --region $REGION
+--create-bucket-configuration
+LocationConstraint=$REGION >> $LOG_FILE 2>&1
+
+echo "Turning off block public access..."
+aws s3api put-public-access-block \
+--bucket $BUCKET_NAME \
+--public-access-block-configuration \
+
+BlockPublicPolicy=false,RestrictPublicBuckets=false >> $LOG_FILE 2>&1
+
+echo "Applying public-read bucket policy..."
+cat > policy.json <<EOL
+{
+  "Version":"2012-10-17",
+  "Statement":[{
+    "Sid":"PublicReadGetObject",
+    "Effect":"Allow",
+    "Principal": "*",
+    "Action":["s3:GetObject"],
+    "Resource":["arn:aws:s3:::$BUCKET_NAME/*"]
+      }]
+}
+EOL
+
+aws s3api put-bucket-policy --bucket $BUCKET_NAME --policy file://policy.json >> $LOG_FILE 2>&1
+
+echo "Uploading test file..."
+echo "This is a test file uploaded using bash automation." > test.txt
+aws s3 cp test.txt s3://$BUCKET_NAME/ >> $LOG_FILE 2>&1
+
+echo "Listing files in the bucket..."
+aws s3 ls s3://$BUCKET_NAME/ | tee -a $LOG_FILE
+
+echo "Downloading the file..."
+aws s3 cp s3://$BUCKET_NAME/test.txt .
+downloaded_test.txt  >> $LOG_FILE 2>&1
+
+echo "Cloud storage setup completed successfully." 
+echo "Check your log file ($LOG_FILE) for details."
+
+
